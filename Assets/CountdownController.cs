777e93f5554186d1eb59fc8b40147acc90fd3af0
Maira Akram <mairaakram04@gmail.com>
2020-07-05 11:52:19 +0500

    Car Racing Game Unity

---
+++ Assets/CountdownController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CountdownController : MonoBehaviour
+{
+    [SerializeField] public Text countdownTextField;
+    public float timeLeft = 3.0f;
+    void Start()
+    {
+        //StartCoroutine(Countdown(4));
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        countdownTextField.text = (timeLeft).ToString();
+        if (timeLeft < 1)
+        {
+            countdownTextField.text = "GO!";
+        }
+        if (timeLeft < 0)
+        {
+            countdownTextField.text = "";
+        }
+    }
+}
---
+++ Assets/FinishLine.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinishLine : MonoBehaviour
+{
+    [SerializeField] private Image image;
+    bool isEnable = false;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        image.enabled = false;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (isEnable == false)
+            {
+                image.enabled = true;
+            }
+            //yield return new WaitForSeconds(2);
+            Time.timeScale = 0;
+        }
+    }
+
+
+        void OnCollisionEnter(Collision collide)
+        {
+            Debug.Log(collide.collider.name);
+            Time.timeScale = 0;
+        }
+}
